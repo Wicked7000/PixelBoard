f76f28f1a3c7664095608ca795cafc25c014f80d
Jason Attwood <jason_attwood@hotmail.co.uk>
2020-08-16 19:44:12 +0100

    link to create issue when you create a cell

--- app/client/components/PixelBoard.tsx
+++ app/client/components/PixelBoard.tsx
@@ -18,19 +18,23 @@ export default class PixelBoard extends React.Component<Props, {}>{
     generateCells(){
         const {pixels} = this.props;
         const elements = [];
+        const bodyText = "If you'd like to modify the colour of the pixel please change the three values after 'pixel|', otherwise submit!";
+        
         for(const pixel of pixels){
-            const colour = pixel.colour;
-            
+            const colour = pixel.colour;            
+            const titleText = `pixel|0,0,0|${pixel.x}|${pixel.y}`;
             elements.push(
-                <div key={`${pixel.x}::${pixel.y}`} style={{
-                    position: "absolute",
-                    backgroundColor: `rgb(${colour[0]},${colour[1]},${colour[2]})`,
-                    left: `${pixel.x === 0 ? 0 : (pixel.x/GRID_SIZE)*100}%`,
-                    top: `${pixel.y === 0 ? 0 : (pixel.y/GRID_SIZE)*100}%`,
-                    width: `${100/GRID_SIZE}%`,
-                    height: `${100/GRID_SIZE}%`
-                }}>
-                </div>
+                <a href={`https://github.com/Wicked7000/PixelBoard/issues/new?title=${encodeURIComponent(titleText)}&body=${encodeURIComponent(bodyText)}`}>
+                    <div className="pixel" key={`${pixel.x}::${pixel.y}`} style={{
+                        position: "absolute",
+                        backgroundColor: `rgb(${colour[0]},${colour[1]},${colour[2]})`,
+                        left: `${pixel.x === 0 ? 0 : (pixel.x/GRID_SIZE)*100}%`,
+                        top: `${pixel.y === 0 ? 0 : (pixel.y/GRID_SIZE)*100}%`,
+                        width: `${100/GRID_SIZE}%`,
+                        height: `${100/GRID_SIZE}%`
+                    }}>
+                    </div>
+                </a>
             )
         }
         return elements;
