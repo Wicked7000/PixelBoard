9b002a9224988965479c341ab5fee3f8c4af08cf
Jason Attwood <jason_attwood@hotmail.co.uk>
2020-08-16 19:03:11 +0100

    Read environment variables directly

--- app/server/server.ts
+++ app/server/server.ts
@@ -9,9 +9,9 @@ const start = async () => {
     const dbHandler = new DatabaseHandler('pixels');
     const pixelHandler = new PixelHandler(dbHandler, GRID_SIZE);
 
-    if(process.argv.length > 2){
-        const userCommand = process.argv[2];
-        const githubUsername = process.argv[3];
+    if(process.env.GITHUB_USER && process.env.ISSUE_TITLE){
+        const userCommand = process.env.ISSUE_TITLE;
+        const githubUsername = process.env.GITHUB_USER;
         const regex = /^pixel|\d+,\d+,\d+|\d+|\d+$/g;
         if(regex.test(userCommand)){
             pixelHandler.updatePixelFromUserCommand(userCommand, githubUsername);
