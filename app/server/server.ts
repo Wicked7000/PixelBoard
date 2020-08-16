3ae8903efbd067bfc2d5b8707dd30c9ae6836a5c
Jason Attwood <jason_attwood@hotmail.co.uk>
2020-08-16 19:54:53 +0100

    Close Issue after pipeline has finished running against it!

--- app/server/server.ts
+++ app/server/server.ts
@@ -1,4 +1,4 @@
-import { promises as fs, constants } from 'fs';
+import { Octokit } from '@octokit/rest';
 import ServerSideRenderer from './ServerSideRenderer';
 import PixelHandler from './PixelHandler';
 import DatabaseHandler from './DatabaseHandler';
@@ -9,7 +9,18 @@ const start = async () => {
     const dbHandler = new DatabaseHandler('pixels');
     const pixelHandler = new PixelHandler(dbHandler, GRID_SIZE);
 
-    if(process.env.GITHUB_USER && process.env.ISSUE_TITLE){
+    if(process.env.GITHUB_USER && process.env.ISSUE_TITLE && process.env.ISSUE_ID && process.env.REPOSITORY_ID){
+        const octokit = new Octokit({
+            auth: process.env.GITHUB_TOKEN
+        });
+        octokit.issues.update({
+            owner: process.env.GITHUB_USER,
+            issue_number: parseInt(process.env.ISSUE_ID, 10),
+            repo: process.env.REPOSITORY_ID,
+            state: 'closed',
+        });
+        console.log('Attempt to close issue!');
+
         const userCommand = process.env.ISSUE_TITLE;
         const githubUsername = process.env.GITHUB_USER;
         const regex = /^pixel|\d+,\d+,\d+|\d+|\d+$/g;
